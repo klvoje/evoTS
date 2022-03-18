4b05a416afbb179573ce6d3b2e2a0d1f34101766
Kjetil Lysne Voje <35102014+klvoje@users.noreply.github.com>
2022-03-18 13:40:55 +0100

    fixed bug
    Fixed bug in make.multivar.evoTS

--- R/make.multivar.evoTS.R
+++ R/make.multivar.evoTS.R
@@ -66,44 +66,44 @@ make.multivar.evoTS<-function (evoTS.1=NULL, evoTS.2=NULL, evoTS.3=NULL, evoTS.4
 
   if(number.of.traits ==5)  {
     xx<-cbind(all.data[[1]]$mm, all.data[[2]]$mm, all.data[[3]]$mm, all.data[[4]]$mm, all.data[[5]]$mm)
-    vv<-cbind(all.data[[1]]$vv, all.data[[2]]$vv, all.data[[3]]$vv, all.data[[4]]$vv, all.data[[4]]$vv)
-    nn<-cbind(all.data[[1]]$nn, all.data[[2]]$nn, all.data[[3]]$nn, all.data[[4]]$nn, all.data[[4]]$nn)
-    tt<-cbind(all.data[[1]]$tt, all.data[[2]]$tt, all.data[[3]]$tt, all.data[[4]]$tt, all.data[[4]]$tt)
+    vv<-cbind(all.data[[1]]$vv, all.data[[2]]$vv, all.data[[3]]$vv, all.data[[4]]$vv, all.data[[5]]$vv)
+    nn<-cbind(all.data[[1]]$nn, all.data[[2]]$nn, all.data[[3]]$nn, all.data[[4]]$nn, all.data[[5]]$nn)
+    tt<-cbind(all.data[[1]]$tt, all.data[[2]]$tt, all.data[[3]]$tt, all.data[[4]]$tt, all.data[[5]]$tt)
   }
 
   if(number.of.traits ==6)  {
     xx<-cbind(all.data[[1]]$mm, all.data[[2]]$mm, all.data[[3]]$mm, all.data[[4]]$mm, all.data[[5]]$mm, all.data[[6]]$mm)
-    vv<-cbind(all.data[[1]]$vv, all.data[[2]]$vv, all.data[[3]]$vv, all.data[[4]]$vv, all.data[[4]]$vv, all.data[[6]]$vv)
-    nn<-cbind(all.data[[1]]$nn, all.data[[2]]$nn, all.data[[3]]$nn, all.data[[4]]$nn, all.data[[4]]$nn, all.data[[6]]$nn)
-    tt<-cbind(all.data[[1]]$tt, all.data[[2]]$tt, all.data[[3]]$tt, all.data[[4]]$tt, all.data[[4]]$tt, all.data[[6]]$tt)
+    vv<-cbind(all.data[[1]]$vv, all.data[[2]]$vv, all.data[[3]]$vv, all.data[[4]]$vv, all.data[[5]]$vv, all.data[[6]]$vv)
+    nn<-cbind(all.data[[1]]$nn, all.data[[2]]$nn, all.data[[3]]$nn, all.data[[4]]$nn, all.data[[5]]$nn, all.data[[6]]$nn)
+    tt<-cbind(all.data[[1]]$tt, all.data[[2]]$tt, all.data[[3]]$tt, all.data[[4]]$tt, all.data[[5]]$tt, all.data[[6]]$tt)
   }
 
   if(number.of.traits ==7)  {
     xx<-cbind(all.data[[1]]$mm, all.data[[2]]$mm, all.data[[3]]$mm, all.data[[4]]$mm, all.data[[5]]$mm, all.data[[6]]$mm, all.data[[7]]$mm)
-    vv<-cbind(all.data[[1]]$vv, all.data[[2]]$vv, all.data[[3]]$vv, all.data[[4]]$vv, all.data[[4]]$vv, all.data[[6]]$vv, all.data[[7]]$vv)
-    nn<-cbind(all.data[[1]]$nn, all.data[[2]]$nn, all.data[[3]]$nn, all.data[[4]]$nn, all.data[[4]]$nn, all.data[[6]]$nn, all.data[[7]]$nn)
-    tt<-cbind(all.data[[1]]$tt, all.data[[2]]$tt, all.data[[3]]$tt, all.data[[4]]$tt, all.data[[4]]$tt, all.data[[6]]$tt, all.data[[7]]$tt)
+    vv<-cbind(all.data[[1]]$vv, all.data[[2]]$vv, all.data[[3]]$vv, all.data[[4]]$vv, all.data[[5]]$vv, all.data[[6]]$vv, all.data[[7]]$vv)
+    nn<-cbind(all.data[[1]]$nn, all.data[[2]]$nn, all.data[[3]]$nn, all.data[[4]]$nn, all.data[[5]]$nn, all.data[[6]]$nn, all.data[[7]]$nn)
+    tt<-cbind(all.data[[1]]$tt, all.data[[2]]$tt, all.data[[3]]$tt, all.data[[4]]$tt, all.data[[5]]$tt, all.data[[6]]$tt, all.data[[7]]$tt)
   }
 
   if(number.of.traits ==8)  {
     xx<-cbind(all.data[[1]]$mm, all.data[[2]]$mm, all.data[[3]]$mm, all.data[[4]]$mm, all.data[[5]]$mm, all.data[[6]]$mm, all.data[[7]]$mm, all.data[[8]]$mm)
-    vv<-cbind(all.data[[1]]$vv, all.data[[2]]$vv, all.data[[3]]$vv, all.data[[4]]$vv, all.data[[4]]$vv, all.data[[6]]$vv, all.data[[7]]$vv, all.data[[8]]$vv)
-    nn<-cbind(all.data[[1]]$nn, all.data[[2]]$nn, all.data[[3]]$nn, all.data[[4]]$nn, all.data[[4]]$nn, all.data[[6]]$nn, all.data[[7]]$nn, all.data[[8]]$nn)
-    tt<-cbind(all.data[[1]]$tt, all.data[[2]]$tt, all.data[[3]]$tt, all.data[[4]]$tt, all.data[[4]]$tt, all.data[[6]]$tt, all.data[[7]]$tt, all.data[[8]]$tt)
+    vv<-cbind(all.data[[1]]$vv, all.data[[2]]$vv, all.data[[3]]$vv, all.data[[4]]$vv, all.data[[5]]$vv, all.data[[6]]$vv, all.data[[7]]$vv, all.data[[8]]$vv)
+    nn<-cbind(all.data[[1]]$nn, all.data[[2]]$nn, all.data[[3]]$nn, all.data[[4]]$nn, all.data[[5]]$nn, all.data[[6]]$nn, all.data[[7]]$nn, all.data[[8]]$nn)
+    tt<-cbind(all.data[[1]]$tt, all.data[[2]]$tt, all.data[[3]]$tt, all.data[[4]]$tt, all.data[[5]]$tt, all.data[[6]]$tt, all.data[[7]]$tt, all.data[[8]]$tt)
   }
 
   if(number.of.traits ==9)  {
     xx<-cbind(all.data[[1]]$mm, all.data[[2]]$mm, all.data[[3]]$mm, all.data[[4]]$mm, all.data[[5]]$mm, all.data[[6]]$mm, all.data[[7]]$mm, all.data[[8]]$mm, all.data[[9]]$mm)
-    vv<-cbind(all.data[[1]]$vv, all.data[[2]]$vv, all.data[[3]]$vv, all.data[[4]]$vv, all.data[[4]]$vv, all.data[[6]]$vv, all.data[[7]]$vv, all.data[[8]]$vv, all.data[[9]]$vv)
-    nn<-cbind(all.data[[1]]$nn, all.data[[2]]$nn, all.data[[3]]$nn, all.data[[4]]$nn, all.data[[4]]$nn, all.data[[6]]$nn, all.data[[7]]$nn, all.data[[8]]$nn, all.data[[9]]$nn)
-    tt<-cbind(all.data[[1]]$tt, all.data[[2]]$tt, all.data[[3]]$tt, all.data[[4]]$tt, all.data[[4]]$tt, all.data[[6]]$tt, all.data[[7]]$tt, all.data[[8]]$tt, all.data[[9]]$tt)
+    vv<-cbind(all.data[[1]]$vv, all.data[[2]]$vv, all.data[[3]]$vv, all.data[[4]]$vv, all.data[[5]]$vv, all.data[[6]]$vv, all.data[[7]]$vv, all.data[[8]]$vv, all.data[[9]]$vv)
+    nn<-cbind(all.data[[1]]$nn, all.data[[2]]$nn, all.data[[3]]$nn, all.data[[4]]$nn, all.data[[5]]$nn, all.data[[6]]$nn, all.data[[7]]$nn, all.data[[8]]$nn, all.data[[9]]$nn)
+    tt<-cbind(all.data[[1]]$tt, all.data[[2]]$tt, all.data[[3]]$tt, all.data[[4]]$tt, all.data[[5]]$tt, all.data[[6]]$tt, all.data[[7]]$tt, all.data[[8]]$tt, all.data[[9]]$tt)
   }
 
   if(number.of.traits ==10)  {
     xx<-cbind(all.data[[1]]$mm, all.data[[2]]$mm, all.data[[3]]$mm, all.data[[4]]$mm, all.data[[5]]$mm, all.data[[6]]$mm, all.data[[7]]$mm, all.data[[8]]$mm, all.data[[9]]$mm, all.data[[10]]$mm)
-    vv<-cbind(all.data[[1]]$vv, all.data[[2]]$vv, all.data[[3]]$vv, all.data[[4]]$vv, all.data[[4]]$vv, all.data[[6]]$vv, all.data[[7]]$vv, all.data[[8]]$vv, all.data[[9]]$vv, all.data[[10]]$vv)
-    nn<-cbind(all.data[[1]]$nn, all.data[[2]]$nn, all.data[[3]]$nn, all.data[[4]]$nn, all.data[[4]]$nn, all.data[[6]]$nn, all.data[[7]]$nn, all.data[[8]]$nn, all.data[[9]]$nn, all.data[[10]]$nn)
-    tt<-cbind(all.data[[1]]$tt, all.data[[2]]$tt, all.data[[3]]$tt, all.data[[4]]$tt, all.data[[4]]$tt, all.data[[6]]$tt, all.data[[7]]$tt, all.data[[8]]$tt, all.data[[9]]$tt, all.data[[10]]$tt)
+    vv<-cbind(all.data[[1]]$vv, all.data[[2]]$vv, all.data[[3]]$vv, all.data[[4]]$vv, all.data[[5]]$vv, all.data[[6]]$vv, all.data[[7]]$vv, all.data[[8]]$vv, all.data[[9]]$vv, all.data[[10]]$vv)
+    nn<-cbind(all.data[[1]]$nn, all.data[[2]]$nn, all.data[[3]]$nn, all.data[[4]]$nn, all.data[[5]]$nn, all.data[[6]]$nn, all.data[[7]]$nn, all.data[[8]]$nn, all.data[[9]]$nn, all.data[[10]]$nn)
+    tt<-cbind(all.data[[1]]$tt, all.data[[2]]$tt, all.data[[3]]$tt, all.data[[4]]$tt, all.data[[5]]$tt, all.data[[6]]$tt, all.data[[7]]$tt, all.data[[8]]$tt, all.data[[9]]$tt, all.data[[10]]$tt)
   }
 
   y <- list(xx = xx, vv = vv, nn = nn, tt = tt)
