3ee1f3746b8f7785611f7e604f51b85027691b3a
klvoje <k.l.voje@gmail.com>
2024-09-26 18:18:32 +0200

    update of tt.uncertainty function

--- R/tt.uncertainty.univariate.models.R
+++ R/tt.uncertainty.univariate.models.R
@@ -29,13 +29,13 @@
 #'@examples
 #'
 #'## Generate a paleoTS object by simulating a time series
-#'y<-paleoTS:::sim.GRW(15,0.04,0.08)
+#'y<-paleoTS:::sim.GRW(20,0.02,0.07)
 #'
 #'##Evaluate the effect of uncertainty in the timing of samples.
-#'time.uncertainty.univariate.models(y, model=c("URW", "GRW"))
+#'tt.uncertainty.univariate.models(y, model=c("URW", "GRW"), iter = 10)
 #'
 
-time.uncertainty.univariate.models<-function(y, mode = "uniform", time.min = NULL, time.max = NULL, sd.time = NULL, model = c("URW","GRW"), pool = TRUE, iter=100){
+tt.uncertainty.univariate.models<-function(y, mode = "uniform", time.min = NULL, time.max = NULL, sd.time = NULL, model = c("URW","GRW"), pool = TRUE, iter=5){
   
   if (is.null(time.min) == TRUE) time.min<-y$tt*0.9
   if (is.null(time.max) == TRUE) time.max<-y$tt*1.1
@@ -43,12 +43,12 @@ time.uncertainty.univariate.models<-function(y, mode = "uniform", time.min = NUL
   if (is.null(sd.time) == TRUE) sd.time<-0.05
   
   # Create lists and assign names
-  result_list <- lapply(model, function(name) {
+  result.list <- lapply(model, function(name) {
     vector(mode = 'list', length = iter)
   })
   
   # Assign model names to each list
-  names(result_list) <- paste0("result.list_", model)
+  names(result.list) <- paste0("result.list_", model)
   
   
   # Creating a new time vector based on assumtions about the error
@@ -59,7 +59,7 @@ time.uncertainty.univariate.models<-function(y, mode = "uniform", time.min = NUL
       if (mode == "uniform") {
       for (j in 1:length(y$tt))
         {
-        new.time[j]<-runif(1, min = time.min[j], max = time.max[j])
+        new.time[j]<-stats::runif(1, min = time.min[j], max = time.max[j])
         }  
       }
       
@@ -67,7 +67,7 @@ time.uncertainty.univariate.models<-function(y, mode = "uniform", time.min = NUL
       if (mode == "normal") {
         for (j in 1:length(y$tt))
         {
-          new.time[j]<-rnorm(1, mean = y$mm[j], sd = sd.time)
+          new.time[j]<-stats::rnorm(1, mean = y$mm[j], sd = sd.time)
         }  
       }
 
@@ -75,19 +75,19 @@ time.uncertainty.univariate.models<-function(y, mode = "uniform", time.min = NUL
       if (all(diff(new.time) < 0) == TRUE) {
         tmp_data_matrix<-cbind(y$mm,y$vv,y$nn,new.time)
         tmp_sorted_matrix <- tmp_data_matrix[order(tmp_data_matrix[, 4]),]
-        y_new_tt<-paleoTS:::as.paleoTS(mm = tmp_sorted_matrix[,1], vv = tmp_sorted_matrix[,2], nn = tmp_sorted_matrix[,3], tt = tmp_sorted_matrix[,4])
+        y_new_tt<-paleoTS::as.paleoTS(mm = tmp_sorted_matrix[,1], vv = tmp_sorted_matrix[,2], nn = tmp_sorted_matrix[,3], tt = tmp_sorted_matrix[,4])
       } 
       
-      if (all(diff(new.time) < 0) == FALSE)  {y_new_tt<-paleoTS:::as.paleoTS(mm = y$mm, vv = y$vv, nn = y$nn, tt = new.time)}
+      if (all(diff(new.time) < 0) == FALSE)  {y_new_tt<-paleoTS::as.paleoTS(mm = y$mm, vv = y$vv, nn = y$nn, tt = new.time)}
       
       # Fitting models based on new time vector
-      if ("Stasis" %in% model) {result_list$result.list_Stasis[[i]]<-paleoTS:::opt.joint.Stasis(y_new_tt, pool = pool)
+      if ("Stasis" %in% model) {result.list$result.list_Stasis[[i]]<-paleoTS::opt.joint.Stasis(y_new_tt, pool = pool)
       }
-      if ("URW" %in% model) {result_list$result.list_URW[[i]]<-paleoTS:::opt.joint.URW(y_new_tt, pool = pool)
+      if ("URW" %in% model) {result.list$result.list_URW[[i]]<-paleoTS::opt.joint.URW(y_new_tt, pool = pool)
       }
-      if ("GRW" %in% model) {result.list$result.list_GRW[[i]]<-paleoTS:::opt.joint.GRW(y_new_tt, pool = pool)
+      if ("GRW" %in% model) {result.list$result.list_GRW[[i]]<-paleoTS::opt.joint.GRW(y_new_tt, pool = pool)
       }
-      if ("OU.fixed" %in% model) {result.list$result.list_OU.fixed[[i]]<-paleoTS:::opt.joint.OU(y_new_tt, pool = pool)
+      if ("OU.fixed" %in% model) {result.list$result.list_OU.fixed[[i]]<-paleoTS::opt.joint.OU(y_new_tt, pool = pool)
       }
       if ("OUBM" %in% model) {result.list$result.list_OUBM[[i]]<-opt.joint.OUBM(y_new_tt, pool = pool)
       }
@@ -99,46 +99,46 @@ time.uncertainty.univariate.models<-function(y, mode = "uniform", time.min = NUL
     }
 
   #Structuring output in lists
-  if ("Stasis" %in% model) {Stasis_aicc_values <- sapply(result_list$result.list_Stasis, function(x) {x$AICc})} else {Stasis_aicc_values<-NULL; } 
-  if ("Stasis" %in% model) {Stasis_logl_values <- sapply(result_list$result.list_Stasis, function(x) {x$logL});
-  Stasis_par_values <- sapply(result_list$result.list_Stasis, function(x) {x$parameters});
-  Stasis.mean.par<-apply(Stasis_par_values,1,mean); Stasis.SD.par<-apply(Stasis_par_values,1,sd) 
+  if ("Stasis" %in% model) {Stasis_aicc_values <- sapply(result.list$result.list_Stasis, function(x) {x$AICc})} else {Stasis_aicc_values<-NULL; } 
+  if ("Stasis" %in% model) {Stasis_logl_values <- sapply(result.list$result.list_Stasis, function(x) {x$logL});
+  Stasis_par_values <- sapply(result.list$result.list_Stasis, function(x) {x$parameters});
+  Stasis.mean.par<-apply(Stasis_par_values,1,mean); Stasis.SD.par<-apply(Stasis_par_values,1,stats::sd) 
   }
   
-  if ("URW" %in% model) {URW_aicc_values <- sapply(result_list$result.list_URW, function(x) {x$AICc})} else {URW_aicc_values<-NULL; } 
-  if ("URW" %in% model) {URW_logl_values <- sapply(result_list$result.list_URW, function(x) {x$logL});
-  URW_par_values <- sapply(result_list$result.list_URW, function(x) {x$parameters});
-  URW.mean.par<-apply(URW_par_values,1,mean); URW.SD.par<-apply(URW_par_values,1,sd) 
+  if ("URW" %in% model) {URW_aicc_values <- sapply(result.list$result.list_URW, function(x) {x$AICc})} else {URW_aicc_values<-NULL; } 
+  if ("URW" %in% model) {URW_logl_values <- sapply(result.list$result.list_URW, function(x) {x$logL});
+  URW_par_values <- sapply(result.list$result.list_URW, function(x) {x$parameters});
+  URW.mean.par<-apply(URW_par_values,1,mean); URW.SD.par<-apply(URW_par_values,1,stats::sd) 
   }
   
   if ("GRW" %in% model) {GRW_aicc_values <- sapply(result.list$result.list_GRW, function(x) {x$AICc})} else {GRW_aicc_values<-NULL; } 
   if ("GRW" %in% model) {GRW_logl_values <- sapply(result.list$result.list_GRW, function(x) {x$logL});
   GRW_par_values <- sapply(result.list$result.list_GRW, function(x) {x$parameters});
-  GRW.mean.par<-apply(GRW_par_values,1,mean); GRW.SD.par<-apply(GRW_par_values,1,sd)
+  GRW.mean.par<-apply(GRW_par_values,1,mean); GRW.SD.par<-apply(GRW_par_values,1,stats::sd)
   }
   
   if ("OU.fixed" %in% model) {OU.fixed_aicc_values <- sapply(result.list$result.list_OU.fixed, function(x) {x$AICc})} else {OU.fixed_aicc_values<-NULL; }
   if ("OU.fixed" %in% model) {OU.fixed_logl_values <- sapply(result.list$result.list_OU.fixed, function(x) {x$logL});
   OU.fixed_par_values <- sapply(result.list$result.list_OU.fixed, function(x) {x$parameters});
-  OU.fixed.mean.par<-apply(OU.fixed_par_values,1,mean); OU.fixed.SD.par<-apply(OU.fixed_par_values,1,sd)
+  OU.fixed.mean.par<-apply(OU.fixed_par_values,1,mean); OU.fixed.SD.par<-apply(OU.fixed_par_values,1,stats::sd)
   }
   
   if ("OUBM" %in% model) {OUBM_aicc_values <- sapply(result.list$result.list_OUBM, function(x) {x$AICc})} else {OUBM_aicc_values<-NULL; }
   if ("OUBM" %in% model) {OUBM_logl_values <- sapply(result.list$result.list_OUBM, function(x) {x$logL});
   OUBM_par_values <- sapply(result.list$result.list_OUBM, function(x) {x$parameters});
-  OUBM.mean.par<-apply(OUBM_par_values,1,mean); OUBM.SD.par<-apply(OUBM_par_values,1,sd)
+  OUBM.mean.par<-apply(OUBM_par_values,1,mean); OUBM.SD.par<-apply(OUBM_par_values,1,stats::sd)
   }
   
   if ("accel" %in% model) {accel_aicc_values <- sapply(result.list$result.list_accel, function(x) {x$AICc})} else {accel_aicc_values<-NULL; }
   if ("accel" %in% model) {accel_logl_values <- sapply(result.list$result.list_accel, function(x) {x$logL});
   accel_par_values <- sapply(result.list$result.list_accel, function(x) {x$parameters});
-  accel.mean.par<-apply(accel_par_values,1,mean); accel.SD.par<-apply(accel_par_values,1,sd)
+  accel.mean.par<-apply(accel_par_values,1,mean); accel.SD.par<-apply(accel_par_values,1,stats::sd)
   }
   
   if ("decel" %in% model) {decel_aicc_values <- sapply(result.list$result.list_decel, function(x) {x$AICc})} else {decel_aicc_values<-NULL; }
   if ("decel" %in% model) {decel_logl_values <- sapply(result.list$result.list_decel, function(x) {x$logL});
   decel_par_values <- sapply(result.list$result.list_decel, function(x) {x$parameters});
-  decel.mean.par<-apply(decel_par_values,1,mean); decel.SD.par<-apply(decel_par_values,1,sd)
+  decel.mean.par<-apply(decel_par_values,1,mean); decel.SD.par<-apply(decel_par_values,1,stats::sd)
   }
   
   # Combine vectors into a list
